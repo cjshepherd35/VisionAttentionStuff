48bb4f2ac19b2b9a66e7f77397665c2c26061039
Colton Shepherd <cshep2012@gmail.com>
2025-07-18 09:19:14 -0500

    light cone att made, very slow using for loops, needs to be sped up

--- lightconeatt.py
+++ lightconeatt.py
@@ -12,10 +12,12 @@ H = 16
 W = 16
 dropout = 0.2
 n_embed = 16
+num_neurons = 32
 block_size = 6
 patch_size = 4 #height and  width must be divisible by patch_size
 max_window = H #max size for patches being put together for each query
 num_patches = 16
+batch_size = 1
 
 def patchify(imgs, patch_size):
     """
@@ -88,113 +90,33 @@ class Head(nn.Module):
 
     def __init__(self, head_size):
         super().__init__()
-        self.query = nn.Linear(n_embed, n_embed, bias=False)
+        self.query = nn.Linear(n_embed, num_neurons, bias=False)
         # self.register_buffer('tril', torch.tril(torch.ones(block_size, block_size)))
         # self.dropout = nn.Dropout(dropout)
         
         self.keys = []
         self.values = []
         for i in range(num_patches):
-            self.keys.append(nn.Linear(n_embed,n_embed))
-            self.values.append(nn.Linear(n_embed,n_embed))
+            self.keys.append(nn.Linear(n_embed,num_neurons))
+            self.values.append(nn.Linear(n_embed,num_neurons))
 
     def forward(self, x):
-        kvpatches = [] #lcpatchify(x, patch_size=patch_size, max_window=max_window)
+        kvpatches = [] 
+        out = torch.zeros(batch_size, block_size, num_patches, num_neurons)
         qpatches = patchify(x, patch_size)
-        print('qp ', qpatches.shape)
         q = self.query(qpatches)
-        for i in range(x.size(1)):
-            kvpatches.append(lcpatchify(x[0,:i+1].unsqueeze(0), patch_size=patch_size, max_window=max_window))
-            for j in range(num_patches):
-                k = self.keys[i](kvpatches[i][j])
-                v = self.values[i](kvpatches[i][j])
-            
-       
-        # wei = []
-        # for i in range(num_patches):
-        #     # print('pa ', patches[i].shape)
-        #     k = self.keys[i](patches[i])
-        #     v = self.values[i](patches[i])
-        #     print('k ', k.shape, ' v ', v.shape)
-        #     print('q ',q.shape)
-            # wei.append(q.T@k)
-            # print('we ', wei[i].shape)
-            
-        # b,t,h, w = x.shape
-        # k = self.key(patches)
-        # q = self.query(x)
-        # #compute attention scores
-        # wei = q @ k.transpose(-2,-1)    #* c**-0.5
-        # wei = wei.masked_fill(self.tril[:t, :t] == 0, float('-inf')) #(b,t,t)
-        # wei = F.softmax(wei, dim=-1)
-        # wei = self.dropout(wei)
-        # #perform weighted aggregation of the values
-        # v = self.value(patches)
-        # out = wei @ v
-        # return out
-
-class MultiheadAttention(nn.Module):
-    def __init__(self,num_heads, head_size):
-        super().__init__()
-        self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
-        self.proj = nn.Linear(n_embed, n_embed)
-        self.dropout = nn.Dropout(dropout)
-    def forward(self, x):
-        out = torch.cat([h(x) for h in self.heads], dim=-1)
-        out = self.dropout(self.proj(out))
+        for l in range(x.size(0)):
+            for i in range(x.size(1)):
+                kvpatches.append(lcpatchify(x[0,:i+1].unsqueeze(0), patch_size=patch_size, max_window=max_window))
+                for j in range(num_patches):
+                    k = self.keys[j](kvpatches[i][j])
+                    v = self.values[j](kvpatches[i][j])
+                    wei = q[0,i,j] @ k.T
+                    out[l,i,j] = wei @ v
+                
         return out
-
-class FeedForward(nn.Module):
-    def __init__(self, n_embed):
-        super().__init__()
-        self.net = nn.Sequential(
-            nn.Linear(n_embed, 4*n_embed),
-            nn.ReLU(), 
-            nn.Linear(4*n_embed, n_embed), 
-            nn.Dropout(dropout)
-        )
-    def forward(self, x):
-        return self.net(x)
-    
-class Block(nn.Module):
-    def __init__(self, n_embed, n_head):
-        super().__init__()
-        head_size = n_embed // n_head
-        self.sa = MultiheadAttention(n_head, head_size)
-        self.ffwd = FeedForward(n_embed)
-        self.ln1 = nn.LayerNorm(n_embed)
-        self.ln2 = nn.LayerNorm(n_embed)
+       
         
-    def forward(self, x):
-        x = x + self.sa(self.ln1(x))
-        x = x + self.ffwd(self.ln2(x))
-        return x
-
-
-
-class LightConeAttention(nn.Module):
-    def __init__(self, dim, num_heads=4):
-        super().__init__()
-        self.position_embedding_table = nn.Embedding(block_size, n_embed)
-        self.num_heads = num_heads
-        self.head_dim = dim // num_heads
-        self.scale = self.head_dim ** -0.5
-
-        self.to_qkv = nn.Linear(dim, dim * 3, bias=False)
-        self.out_proj = nn.Linear(dim, dim)
-        self.mask = self._light_cone_mask(tq, hq, wq, T, H, W)
-
-    
-    def forward(self, x):
-        # x: (B, T, H, W, C)
-        B, T, H, W, C = x.shape
-        qkv = self.to_qkv(x).reshape(B, T, H, W, 3, self.num_heads, self.head_dim)
-        q, k, v = qkv.unbind(dim=4)  # each: (B, T, H, W, num_heads, head_dim)
-
-        out = torch.zeros_like(q)
-
-
-
 
 
     def _light_cone_mask(self, t_q, h_q, w_q, T, H, W, device='cpu'):
@@ -225,4 +147,4 @@ print('x ', x.shape)
 head = Head(head_size=32)
 output = head(x)
 
-
+print('output ', output.shape)
